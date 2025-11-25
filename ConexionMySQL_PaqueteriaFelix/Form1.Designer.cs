daf3236c49f8bf66c678e2bfe2d1121ede79958e
Alejandro Guzman <alejandroguzca@gmail.com>
2025-11-24 20:06:16 -0600

    Add project files.

---
+++ ConexionMySQL_PaqueteriaFelix/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ConexionMySQL_PaqueteriaFelix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Conectar = new System.Windows.Forms.Button();
+            this.Consultar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panelCampos = new System.Windows.Forms.Panel();
+            this.buttonInsertar = new System.Windows.Forms.Button();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Conectar
+            // 
+            this.Conectar.Location = new System.Drawing.Point(28, 70);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(190, 60);
+            this.Conectar.TabIndex = 0;
+            this.Conectar.Text = "Conectar";
+            this.Conectar.UseVisualStyleBackColor = true;
+            this.Conectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Consultar
+            // 
+            this.Consultar.Location = new System.Drawing.Point(234, 70);
+            this.Consultar.Name = "Consultar";
+            this.Consultar.Size = new System.Drawing.Size(190, 60);
+            this.Consultar.TabIndex = 1;
+            this.Consultar.Text = "Consultar";
+            this.Consultar.UseVisualStyleBackColor = true;
+            this.Consultar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 148);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(652, 199);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 16;
+            this.comboBox1.Location = new System.Drawing.Point(445, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(235, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panelCampos
+            // 
+            this.panelCampos.Location = new System.Drawing.Point(707, 148);
+            this.panelCampos.Name = "panelCampos";
+            this.panelCampos.Size = new System.Drawing.Size(340, 377);
+            this.panelCampos.TabIndex = 4;
+            this.panelCampos.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCampos_Paint);
+            // 
+            // buttonInsertar
+            // 
+            this.buttonInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonInsertar.Location = new System.Drawing.Point(707, 70);
+            this.buttonInsertar.Name = "buttonInsertar";
+            this.buttonInsertar.Size = new System.Drawing.Size(340, 60);
+            this.buttonInsertar.TabIndex = 5;
+            this.buttonInsertar.Text = "Insertar";
+            this.buttonInsertar.UseVisualStyleBackColor = true;
+            this.buttonInsertar.UseWaitCursor = true;
+            this.buttonInsertar.Click += new System.EventHandler(this.buttonInsertar_Click_1);
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.buttonGuardar.Location = new System.Drawing.Point(28, 382);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(351, 174);
+            this.buttonGuardar.TabIndex = 6;
+            this.buttonGuardar.Text = "Guardar cambios (update)";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.buttonEliminar.Location = new System.Drawing.Point(385, 382);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(295, 174);
+            this.buttonEliminar.TabIndex = 7;
+            this.buttonEliminar.Text = "Eliminar Tupla";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 642);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.buttonInsertar);
+            this.Controls.Add(this.panelCampos);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Consultar);
+            this.Controls.Add(this.Conectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.Button Consultar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panelCampos;
+        private System.Windows.Forms.Button buttonInsertar;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonEliminar;
+    }
+}
+
---
+++ ConexionMySQL_PaqueteriaFelix/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ConexionMySQL_PaqueteriaFelix
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conexion;
+        private MySqlDataAdapter adaptador;
+        private DataTable tablaGlobal;
+        string cadenaConexion = "Server=localhost; Database=The Gyatt Plex; Uid=root; Pwd=root;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+                MessageBox.Show("Conexión exitosa con la base de datos MySQL.");
+
+                // Cargar nombres de tablas en el ComboBox
+                string query = "SHOW TABLES;";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                comboBox1.Items.Clear();
+
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader[0].ToString());
+                }
+
+                reader.Close();
+                conexion.Close();
+
+                if (comboBox1.Items.Count > 0)
+                    comboBox1.SelectedIndex = 0; // Seleccionar primera tabla automáticamente
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" Error: " + ex.Message);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Selecciona una tabla primero.");
+                return;
+            }
+
+            try
+            {
+                string tablaSeleccionada = comboBox1.SelectedItem.ToString();
+
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+
+                string consulta = $"SELECT * FROM `{tablaSeleccionada}`;";
+                MySqlCommand comando = new MySqlCommand(consulta, conexion);
+
+                adaptador = new MySqlDataAdapter(comando);
+                MySqlCommandBuilder builder = new MySqlCommandBuilder(adaptador);
+
+                tablaGlobal = new DataTable();
+                adaptador.Fill(tablaGlobal);
+
+                dataGridView1.DataSource = tablaGlobal;
+
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar datos: " + ex.Message);
+            }
+        }
+
+        private void CargarCamposDeTabla()
+        {
+            if (comboBox1.SelectedItem == null) return;
+
+            string tabla = comboBox1.SelectedItem.ToString();
+            panelCampos.Controls.Clear();
+
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+
+                string query = $"DESCRIBE `{tabla}`;";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                int y = 10;
+
+                while (reader.Read())
+                {
+                    string nombreColumna = reader.GetString("Field");
+
+                    Label lbl = new Label();
+                    lbl.Text = nombreColumna;
+                    lbl.Location = new Point(10, y);
+                    panelCampos.Controls.Add(lbl);
+
+                    TextBox txt = new TextBox();
+                    txt.Name = "txt_" + nombreColumna;
+                    txt.Location = new Point(120, y);
+                    txt.Width = 150;
+
+                    panelCampos.Controls.Add(txt);
+
+                    y += 30;
+                }
+
+                reader.Close();
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error cargando columnas: " + ex.Message);
+            }
+        }
+
+        private string ObtenerPK(string tabla)
+        {
+            string pk = null;
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+                string query = $"SHOW KEYS FROM `{tabla}` WHERE Key_name = 'PRIMARY';";
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    pk = reader.GetString("Column_name");
+                }
+
+                reader.Close();
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener PK: " + ex.Message);
+            }
+
+            return pk;
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dataGridView1.Rows[e.RowIndex];
+
+                foreach (Control control in panelCampos.Controls)
+                {
+                    if (control is TextBox txt)
+                    {
+                        string columna = txt.Name.Replace("txt_", "");
+
+                        if (fila.Cells[columna].Value != null)
+                            txt.Text = fila.Cells[columna].Value.ToString();
+                    }
+                }
+            }
+        }
+
+        private void buttonInsertar_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Selecciona una tabla.");
+                return;
+            }
+
+            string tabla = comboBox1.SelectedItem.ToString();
+
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+
+                // Construir INSERT dinámico
+                List<string> columnas = new List<string>();
+                List<string> valores = new List<string>();
+
+                foreach (Control control in panelCampos.Controls)
+                {
+                    if (control is TextBox txt)
+                    {
+                        string columna = txt.Name.Replace("txt_", "");
+                        columnas.Add($"`{columna}`");
+
+                        // Preparar valores con comillas
+                        valores.Add($"'{txt.Text}'");
+                    }
+                }
+
+                string query =
+                    $"INSERT INTO `{tabla}` ({string.Join(",", columnas)}) VALUES ({string.Join(",", valores)});";
+
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Registro insertado correctamente.");
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error insertando: " + ex.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarCamposDeTabla();
+        }
+
+        private void panelCampos_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void buttonInsertar_Click_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Selecciona una tabla.");
+                return;
+            }
+
+            string tabla = comboBox1.SelectedItem.ToString();
+
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+
+                // Construir INSERT dinámico
+                List<string> columnas = new List<string>();
+                List<string> valores = new List<string>();
+
+                foreach (Control control in panelCampos.Controls)
+                {
+                    if (control is TextBox txt)
+                    {
+                        string columna = txt.Name.Replace("txt_", "");
+                        columnas.Add($"`{columna}`");
+
+                        // Preparar valores con comillas
+                        valores.Add($"'{txt.Text}'");
+                    }
+                }
+
+                string query =
+                    $"INSERT INTO `{tabla}` ({string.Join(",", columnas)}) VALUES ({string.Join(",", valores)});";
+
+                MySqlCommand cmd = new MySqlCommand(query, conexion);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Registro insertado correctamente.");
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error insertando: " + ex.Message);
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGuardar_Click(object sender, EventArgs e)
+        {
+            if (tablaGlobal == null)
+            {
+                MessageBox.Show("No hay datos cargados para guardar.");
+                return;
+            }
+
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+
+                adaptador.Update(tablaGlobal);
+
+                MessageBox.Show("Cambios guardados correctamente.");
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar los cambios: " + ex.Message);
+            }
+        }
+
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null || dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Selecciona una tabla y una fila para eliminar.");
+                return;
+            }
+
+            string tabla = comboBox1.SelectedItem.ToString();
+            DataGridViewRow fila = dataGridView1.CurrentRow;
+
+            try
+            {
+                string pkNombre = tablaGlobal.Columns[0].ColumnName; // Primera columna como PK
+                object pkValor = fila.Cells[pkNombre].Value;
+
+                bool tieneIsActive = tablaGlobal.Columns.Contains("IsActive");
+
+                string query;
+
+                if (tieneIsActive)
+                {
+                    query = $"UPDATE `{tabla}` SET `IsActive` = 0 WHERE `{pkNombre}` = @valor;";
+                }
+                else
+                {
+                    query = $"DELETE FROM `{tabla}` WHERE `{pkNombre}` = @valor;";
+                }
+
+                using (MySqlConnection con = new MySqlConnection(cadenaConexion))
+                {
+                    con.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@valor", pkValor);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+                // Ahora sí eliminamos la fila del DataTable
+                tablaGlobal.Rows.RemoveAt(fila.Index);
+
+                MessageBox.Show(tieneIsActive
+                    ? "Registro desactivado correctamente."
+                    : "Registro eliminado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar/desactivar: " + ex.Message);
+            }
+        }
+
+    }
+}
